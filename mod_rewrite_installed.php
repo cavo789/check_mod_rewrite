7c5a51e8bae1dbc03dc98e690650a4522e671c09
Christophe <cavo789@yahoo.com>
2019-04-16 18:14:06 +0200

    Improve layout with Bulma.css

--- mod_rewrite_installed.php
+++ mod_rewrite_installed.php
@@ -1,3 +1,28 @@
-<?php
+<!DOCTYPE html>
+<html>
 
-echo "mod_rewrite is enabled on your server";
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>mod_rewrite</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+</head>
+
+<body>
+    <section class="section">
+        <div class="container">
+            <article class="message is-success">
+                <div class="message-header">
+                    <p>Success</p>
+                    <button class="delete" aria-label="delete"></button>
+                </div>
+                <div class="message-body">
+                    Congrats, mod_rewrite is <strong>enabled</strong> on your server
+                </div>
+            </article>
+        </div>
+    </section>
+</body>
+
+</html>
